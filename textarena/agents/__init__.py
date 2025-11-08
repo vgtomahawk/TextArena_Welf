ab9107c06ee1da437200325f153553a29280a06d
Varun Gangal <varun.gangal@patronus.ai>
2025-11-08 12:12:45 -0500

    Updating init to have LiteLLM Agent

--- textarena/agents/__init__.py
+++ textarena/agents/__init__.py
@@ -1,4 +1,4 @@
 # Agents
-from textarena.agents.basic_agents import HumanAgent, OpenRouterAgent, GeminiAgent, OpenAIAgent, HFLocalAgent, CerebrasAgent, AWSBedrockAgent, AnthropicAgent, GroqAgent, OllamaAgent, LlamaCppAgent
+from textarena.agents.basic_agents import HumanAgent, OpenRouterAgent, GeminiAgent, OpenAIAgent, HFLocalAgent, CerebrasAgent, AWSBedrockAgent, AnthropicAgent, GroqAgent, OllamaAgent, LlamaCppAgent, LiteLLMAgent
 
-__all__ = ["HumanAgent", "OpenRouterAgent", "GeminiAgent", "OpenAIAgent", "HFLocalAgent", "CerebrasAgent", "AWSBedrockAgent", "AnthropicAgent", "GroqAgent", "OllamaAgent", "LlamaCppAgent"]
+__all__ = ["HumanAgent", "OpenRouterAgent", "GeminiAgent", "OpenAIAgent", "HFLocalAgent", "CerebrasAgent", "AWSBedrockAgent", "AnthropicAgent", "GroqAgent", "OllamaAgent", "LlamaCppAgent","LiteLLMAgent"]
